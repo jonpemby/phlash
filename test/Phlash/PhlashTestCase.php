bb7a3f62004ef57ddf07ca7e11bf3e8ddff6c75a
Jonathon Pemberton <jonathon@judson.biz>
2018-10-03 14:31:03 -0500

    set up skeleton for testing

---
+++ test/Phlash/PhlashTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class PhlashTestCase extends TestCase {
+    /**
+     * @return void
+     */
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
+}

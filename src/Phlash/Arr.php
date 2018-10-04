6affa262f14418d527887c99be1bf7a501aeb618
Jonathon Pemberton <jonathon@judson.biz>
2018-10-04 08:43:39 -0500

    even more Arr methods

--- src/Phlash/Arr.php
+++ src/Phlash/Arr.php
@@ -142,6 +142,8 @@ class Arr implements Countable, ArrayAccess, Iterator, JsonSerializable
         $arr = $this->value;
 
         while (true) {
+            if (! count($arr)) break;
+
             if ($fn($arr[0])) {
                 array_shift($arr);
             } else {
@@ -160,6 +162,8 @@ class Arr implements Countable, ArrayAccess, Iterator, JsonSerializable
         $arr = $this->value;
 
         while (true) {
+            if (! count($arr)) break;
+
             if ($fn($arr[count($arr) - 1])) {
                 array_pop($arr);
             } else {
@@ -168,6 +172,183 @@ class Arr implements Countable, ArrayAccess, Iterator, JsonSerializable
         }
 
         return new Arr($arr);
+    }
+
+    /**
+     * Fill the collection with the value from the $start parameter to the $end.
+     *
+     * @param  mixed $value  Value with which to fill
+     * @param  int $start  Start index
+     * @param  int $end  End index
+     * @return Arr
+     */
+    public function fill($value = null, $start = 0, $end = null)
+    {
+        $arr = $this->value;
+
+        $end = $end ?: count($arr);
+
+        for ($i = $start; $i < $end; $i += 1)
+            $arr[$i] = $value;
+
+        return new Arr($arr);
+    }
+
+    /**
+     * Find the first value in the index matched by the $fn
+     *
+     * @param  callable $fn  Predicate by which to match
+     * @return mixed
+     */
+    public function find(callable $fn)
+    {
+        $found = null;
+
+        foreach ($this->value as $v) {
+            if ($fn($v)) {
+                $found = $v;
+                break;
+            }
+        }
+
+        return $found;
+    }
+
+    /**
+     * Find the index of the value matched by the $fn.
+     *
+     * @param  callable  $fn  Predicate by which to match
+     * @return int
+     */
+    public function findIndex(callable $fn)
+    {
+        $index = -1;
+
+        foreach ($this->value as $i => $v) {
+            if ($fn($v)) {
+                $index = $i;
+                break;
+            }
+        }
+
+        return $index;
+    }
+
+    /**
+     * Find the last index of the value matched by the $fn.
+     *
+     * @param  callable $fn
+     * @return int
+     */
+    public function findLastIndex(callable $fn)
+    {
+        $index = -1;
+
+        for ($i = count($this->value) - 1; $i > -1; $i -= 1) {
+            if ($fn($this->value[$i])) {
+                $index = $i;
+                break;
+            }
+        }
+
+        return $index;
+    }
+
+    /**
+     * Retrieve the first value from the Arr.
+     *
+     * @return mixed
+     */
+    public function first()
+    {
+        return $this->value[0] ?: null;
+    }
+
+    /**
+     * Create a new Arr from a given initial value.
+     *
+     * @param  array  $value  Initial value of the Arr
+     * @return Arr
+     */
+    public static function from(array $value = [])
+    {
+        return new static($value);
+    }
+
+    /**
+     * @alias first
+     */
+    public function head()
+    {
+        return $this->first();
+    }
+
+    /**
+     * Find the first index where the value matches the provided $value.
+     *
+     * @param  mixed $value
+     * @return int
+     */
+    public function index($value)
+    {
+        $arr = $this->value;
+
+        for ($i = 0; $i < count($arr); $i += 1)
+            if ($arr[$i] === $value) return $i;
+
+        return -1;
+    }
+
+    /**
+     * Get all but the last element of the Arr.
+     *
+     * @return Arr
+     */
+    public function initial()
+    {
+        $arr = $this->value;
+
+        array_pop($arr);
+
+        return new Arr($arr);
+    }
+
+    /**
+     * Get the last element of the Arr.
+     *
+     * @return mixed
+     */
+    public function last()
+    {
+        return $this->value[count($this->value) - 1];
+    }
+
+    /**
+     * @alias last
+     */
+    public function tail()
+    {
+        return $this->last();
+    }
+
+    /**
+     * Reverse the Arr into a new Arr.
+     *
+     * @return Arr
+     */
+    public function reverse()
+    {
+        return new Arr(array_reverse($this->value));
+    }
+
+    /**
+     * Get the raw value of the Arr.
+     *
+     * @return array
+     */
+    public function value() : array
+    {
+        return $this->value;
     }
 
     /**
--- test/Phlash/ArrTest.php
+++ test/Phlash/ArrTest.php
@@ -128,6 +128,17 @@ class ArrTest extends TestCase
         $this->assertContains(6, $dropped);
     }
 
+    public function testArrDropWhileDoesNotInfiniteLoopOrError()
+    {
+        $array = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $dropped = $array->dropWhile(function ($v) {
+            return true;
+        });
+
+        $this->assertSame([], $dropped->value());
+    }
+
     public function testDropRightWhile()
     {
         $array = new Arr([1, 2, 3, 4, 5, 6]);
@@ -141,5 +152,112 @@ class ArrTest extends TestCase
         $this->assertContains(1, $dropped);
         $this->assertContains(2, $dropped);
         $this->assertContains(3, $dropped);
+    }
+
+    public function testArrDropRightWhileDoesNotInfiniteLoopOrError()
+    {
+        $array = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $dropped = $array->dropRightWhile(function ($v) {
+            return true;
+        });
+
+        $this->assertSame([], $dropped->value());
+    }
+
+    public function testFill()
+    {
+        $array = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $filled = $array->fill(null, 3);
+
+        $this->assertCount(6, $filled);
+
+        $this->assertEquals(1, $filled->at(0));
+        $this->assertEquals(2, $filled->at(1));
+        $this->assertEquals(3, $filled->at(2));
+
+        $this->assertNull($filled->at(3));
+        $this->assertNull($filled->at(4));
+        $this->assertNull($filled->at(5));
+    }
+
+    public function testFind()
+    {
+        $array = new Arr([1, 2, 3, 7, 4, 5, 6]);
+
+        $found = $array->find(function ($v) {
+            return $v === 7;
+        });
+
+        $this->assertEquals(7, $found);
+    }
+
+    public function testFindIndex()
+    {
+        $array = new Arr([1, 2, 3, 7, 4, 5, 6]);
+
+        $found = $array->findIndex(function ($v) {
+            return $v === 7;
+        });
+
+        $this->assertEquals(3, $found);
+    }
+
+    public function testFindLastIndex()
+    {
+        $array = new Arr([1, 2, 3, 7, 4, 5, 7, 6]);
+
+        $found = $array->findLastIndex(function ($v) {
+            return $v === 7;
+        });
+
+        $this->assertEquals(6, $found);
+    }
+
+    public function testFirst()
+    {
+        $array = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $this->assertEquals(1, $array->first());
+        $this->assertEquals(1, $array->head()); // @alias
+    }
+
+    public function testFrom()
+    {
+        $array = Arr::from([1, 2, 3, 4, 5, 6]);
+
+        $other = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $this->assertSame($other->value(), $array->value());
+    }
+
+    public function testIndex()
+    {
+        $array = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $this->assertEquals(2, $array->index(3));
+    }
+
+    public function testInitial()
+    {
+        $array = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $this->assertSame([1, 2, 3, 4, 5], $array->initial()->value());
+    }
+
+    public function testLast()
+    {
+        $array = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $this->assertEquals(6, $array->last());
+        $this->assertEquals(6, $array->tail());
+    }
+
+    public function testReverse()
+    {
+        $array = new Arr([1, 2, 3, 4, 5, 6]);
+
+        $this->assertSame([6, 5, 4, 3, 2, 1], $array->reverse()->value());
     }
 }

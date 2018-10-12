4acf491a7dff755f645132d13870ab4e51648989
Jonathon Pemberton <jonpemby@icloud.com>
2018-10-12 15:30:40 -0500

    create a new Num class to hold numeric methods (#98)
    (#37) create a new Num class to hold numeric methods

--- test/Phlash/PhlashTest.php
+++ test/Phlash/PhlashTest.php
@@ -5,6 +5,7 @@ namespace Phlash\Tests;
 use Phlash\Arr;
 use Phlash\Obj;
 use Phlash\Str;
+use Phlash\Num;
 use function Phlash\phlash;
 
 class PhlashTest extends TestCase
@@ -15,6 +16,7 @@ class PhlashTest extends TestCase
         $this->assertInstanceOf(Obj::class, phlash(['foo' => 'bar']));
         $this->assertInstanceOf(Obj::class, phlash(new \stdClass));
         $this->assertInstanceOf(Str::class, phlash('hello world'));
+        $this->assertInstanceOf(Num::class, phlash(6));
     }
 
     public function testDynamicMethodCalls()
